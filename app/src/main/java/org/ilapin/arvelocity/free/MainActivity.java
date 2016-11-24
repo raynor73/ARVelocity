8a2cc73392f1f2bef0f1c135ba8444b2ee9582c1
Igor Lapin <ilapin@parallels.com>
2016-11-24 18:29:39 +0300

    Add java8 and java streams support

--- app/src/main/java/org/ilapin/arvelocity/free/MainActivity.java
+++ app/src/main/java/org/ilapin/arvelocity/free/MainActivity.java
@@ -1,7 +1,7 @@
 package org.ilapin.arvelocity.free;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
